d63e054ae4b96a7b11f9adc8352345cdb6e75d0b
Raul E. Pena <32050499+RaulEP@users.noreply.github.com>
2022-09-29 05:14:15 +0900

    organized main.py to better make tests

--- main.py
+++ main.py
@@ -3,13 +3,53 @@ import gym
 import highway_env
 import matplotlib.pyplot as pt
 from highway_env import utils
-
-#print(utils.lmap(25, [20,30],[0,1]))
-
+from stable_baselines3 import DQN
+from tqdm.notebook import trange
+import os
 
 env = gym.make('ma-highway-v0')
+DQN_path = os.path.join('Training', 'Saved Models', 'DQN_model')
+
+env.configure({"vehicles_count": 25, "vehicles_density": 1.3, "simulation_frequency":15, "duration":40, "road_length":2000})
+env.reset()
 
 
+#MODEL CREATION
+TRAIN = True
+model = DQN('MlpPolicy', env,
+            policy_kwargs=dict(net_arch=[256, 256]),
+            learning_rate=5e-4,
+            buffer_size=15000,
+            learning_starts=200,
+            batch_size=32,
+            gamma=0.8,
+            train_freq=1,
+            gradient_steps=1,
+            target_update_interval=50,
+            verbose=1,
+            tensorboard_log="highway_dqn/",
+            exploration_fraction=0.7)
+
+#TRAINING
+if TRAIN:
+    model.learn(total_timesteps=int(3e4))
+    model.save(DQN_path)
+    #del model
+
+"""
+###MODEL TESTING###
+model = DQN.load('highway_dqn/model', env=env)
+for episode in trange(10, desc="Test episodes"):
+    obs, done = env.reset(), False
+    while not done:
+        action, _ = model.predict(obs, deterministic=True)
+        obs, reward, done, info = env.step(int(action))
+        env.render('human')
+env.close()
+
+
+
+###TESTING CODE#####
 for trials in range(1):
     env.configure({'show_trajectories':True, 'initial_lane_id':2, 'lanes_count':3, "vehicles_count": 1, })
     terminated = False
@@ -20,54 +60,4 @@ for trials in range(1):
         env.render(mode='human')
         pprint([obs, reward, terminated, info])
 env.close()
-
-"""x = env.render('rgb_array')
-pt.plot(x)
-"""
-
-"""
-TRAIN = True
-
-if __name__ == '__main__':
-    # Create the environment
-    env = gym.make("highway-fast-v0")
-    obs = env.reset()
-
-    # Create the model
-    model = DQN('MlpPolicy', env,
-                policy_kwargs=dict(net_arch=[256, 256]),
-                learning_rate=5e-4,
-                buffer_size=15000,
-                learning_starts=200,
-                batch_size=32,
-                gamma=0.8,
-                train_freq=1,
-                gradient_steps=1,
-                target_update_interval=50,
-                verbose=1,
-                tensorboard_log="highway_dqn/")
-
-    # Train the model
-    if TRAIN:
-        model.learn(total_timesteps=int(2e4))
-        model.save("highway_dqn/model")
-        del model
-
-    # Run the trained model and record video
-    model = DQN.load("highway_dqn/model", env=env)
-    env = RecordVideo(env, video_folder="racetrack_ppo/videos", episode_trigger=lambda e: True)
-    env.unwrapped.set_record_video_wrapper(env)
-    env.configure({"simulation_frequency": 15})  # Higher FPS for rendering
-
-    for videos in range(10):
-        done = False
-        obs = env.reset()
-        while not done:
-            # Predict
-            action, _states = model.predict(obs, deterministic=True)
-            # Get reward
-            obs, reward, done, info = env.step(action)
-            # Render
-            env.render()
-    env.close()
 """
