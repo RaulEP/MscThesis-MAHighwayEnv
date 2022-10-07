49e62c41f823f9314cda3df8499821e97b869178
Raul E. Pena <32050499+RaulEP@users.noreply.github.com>
2022-10-07 16:08:41 +0900

    Added files for testing

--- main.py
+++ main.py
@@ -11,46 +11,46 @@ env = gym.make('ma-highway-v0')
 env.configure(  {
                 "speed_limit": 20,
                 "vehicles_density": 1,
-                "ego_spacing": 1,
+                "ego_spacing": 1.5,
                 "road_length":2000, 
-                "simulation_frequency":15, 
+                "simulation_frequency":60, 
                 "duration":40,
+                "screen_width": 1000,
                 "DLC_config": {
-                    "count": 5,
+                    "count": 3,
                     "reward_speed_range": [20, 40],
                     "weights": [10,5,1,1],
                         },
                 "MLC_config": {
-                    "count":10 ,
+                    "count":5 ,
                     "reward_speed_range": [20, 30],
                     "weights": [2,10,1,1]
                         } 
                 })
 
-DQN_path = os.path.join('Training', 'Saved Models', 'DQN_model')
+A2C_path = os.path.join('Training', 'Saved Models', 'A2C_model')
 
-
+"""
 #MODEL CREATION
 TRAIN = True
-model = PPO('MultiInputPolicy', env,
+model = A2C('MultiInputPolicy', env,
             policy_kwargs=dict(net_arch=[256, 256]),
             learning_rate=5e-4,
             gamma=0.8,
             verbose=1,
-            tensorboard_log="highway_dqn/",
+            tensorboard_log="highway_a2c/",
             normalize_advantage=True)
 
 #TRAINING
 if TRAIN:
-    model
-    model.learn(total_timesteps=int(10))
-    model.save(DQN_path)
+    model.learn(total_timesteps=int(1000))
+    model.save(A2C_path)
     #del model
+"""
 
-
-
+"""
 ###MODEL TESTING###
-model = PPO.load('Training/Saved Models/DQN_model', env=env)
+model = PPO.load('Training/Saved Models/A2C_model', env=env)
 for episode in trange(3, desc="Test episodes"):
     obs, done = env.reset(), False
     while not done:
@@ -59,10 +59,10 @@ for episode in trange(3, desc="Test episodes"):
         print(info)
         env.render('human')
 env.close()
-
 """
+
 ###TESTING CODE#####
-for trials in range(10):
+for trials in range(50):
     terminated = False
     obs = env.reset()
     while not terminated:
@@ -72,4 +72,3 @@ for trials in range(10):
         pprint([obs, reward, terminated])
 env.close()
 
-"""
